d10c4f8024ac34b4d961aa8b0144afb5aa8c912d
artshpakov <artshpakov@yandex.ru>
2014-04-05 14:04:53 +0400

    Deploying current branch on development

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,4 +1,4 @@
-set :branch, "development"
+set :branch, %x{ git symbolic-ref HEAD 2>/dev/null }.chomp.sub("refs/heads/", '') rescue :development
 
 set :domain, "162.243.88.75"
 server domain, :web, :app, :db, primary: true
