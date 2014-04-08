34165c9748c6d351d37246d0c4a14239fd847b63
codesnik <aronaxis@gmail.com>
2014-04-08 14:46:36 +0400

    Yay, a hostname for the dev server

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,4 +1,4 @@
 set :branch, %x{ git symbolic-ref HEAD 2>/dev/null }.chomp.sub("refs/heads/", '') rescue :development
 
-set :domain, "162.243.88.75"
+set :domain, "dev.solverclub.com"
 server domain, :web, :app, :db, primary: true
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 set :branch, "master"
 
-set :domain, "162.243.88.75"
+set :domain, "dev.solverclub.com"
 server domain, :web, :app, :db, primary: true
