cfcccedb606595f23c44743ba4e4b93da4aba9d0
codesnik <aronaxis@gmail.com>
2014-04-13 21:17:29 +0400

    Complete using ES _suggest endpoint

--- app/chewy/complete_index.rb
+++ app/chewy/complete_index.rb
@@ -11,24 +11,73 @@ class CompleteIndex < Chewy::Index
   #   }
   # }'
 
+  def self.complete(text, size: 5)
+    client.suggest(
+      index: 'complete',
+      body: {
+        results: {
+          text: text,
+          completion: {
+            field: "suggest",
+            size: size,
+            fuzzy: { edit_distance: 2}
+          }
+        }
+      }
+    )
+  end
+
+  # FIXME batch-load objects from same tables, if we stick with the implementation
+  def self.complete_loaded(*args)
+    results = complete(*args)["results"][0]["options"]
+    results.map do |o|
+      case o["payload"]["type"]
+      when "realm";       ::Realm
+      when "alternative"; ::Alternative
+      when "criterion";   ::Criterion
+      when "field";       ::Property::Field
+      end.find( o["payload"]["id"] )
+    end
+  end
+
   define_type Realm do
-    field :suggest, type: 'completion', payloads: true, value: -> { name }
+    field :suggest, type: 'completion', payloads: true, value: -> {{
+      weight: 3,
+      output: name,
+      input: [name],
+      payload: { type: 'realm', id: id }
+    }}
   end
 
   define_type Alternative do
     field :name
     field :realm_id
-    field :suggest, type: 'completion', payloads: true, value: -> { name }
+    field :suggest, type: 'completion', payloads: true, value: -> {{
+      weight: 1,
+      output: name,
+      input: [name],
+      payload: { type: 'alternative', id: id }
+    }}
   end
 
   define_type Criterion do
     field :name
-    field :suggest, type: 'completion', payloads: true, value: -> { name }
+    field :suggest, type: 'completion', payloads: true, value: -> {{
+      weight: 2,
+      output: name,
+      input: [name],
+      payload: { type: 'criterion', id: id }
+    }}
   end
 
   define_type Property::Field, name: 'fields' do
     field :name
-    field :suggest, type: 'completion', payloads: true, value: -> { name }
+    field :suggest, type: 'completion', payloads: true, value: -> {{
+      weight: 2,
+      output: name,
+      input: [name],
+      payload: { type: 'field', id: id }
+    }}
   end
 
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
   respond_to :json
 
   def index
-    @results = CompleteIndex.query(regexp: {name: "#{params[:q].downcase}.*"}).load
+    @results = CompleteIndex.complete_loaded(params[:q], size: 8)
   end
 
 end
