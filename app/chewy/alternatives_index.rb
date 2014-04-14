cd0b13fcb37d61d22c94d809c9a618f3ad73f59b
codesnik <aronaxis@gmail.com>
2014-04-14 05:33:07 +0400

    Correct types in AlternativesIndex

--- app/chewy/alternatives_index.rb
+++ app/chewy/alternatives_index.rb
@@ -1,15 +1,16 @@
 class AlternativesIndex < Chewy::Index
   define_type Alternative.includes(:alternatives_criteria, :property_values) do
     field :name
-    field :realm_id
-    
+    field :realm_id, type: 'integer'
+
+    # FIXME get rid of "null" values in indexed documents.
     Criterion.where.not(ancestry: nil).find_each do |cr|
-      field "cr_#{cr.id}".to_sym, 
+      field :"cr_#{cr.id}", type: 'double',
         value: -> { alternatives_criteria.select {|ac| ac.criterion_id == cr.id}.map(&:rating).first }
     end
 
     Property::Field.find_each do |prop|
-      field "prop_#{prop.id}".to_sym,
+      field :"prop_#{prop.id}", type: 'boolean',
         value: -> { property_values.select {|ap| ap.field_id == prop.id}.map(&:value).first }
     end
   end
