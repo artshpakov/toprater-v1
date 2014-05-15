ebb84131ded44d98b841e3d24390bf389bac6676
codesnik <aronaxis@gmail.com>
2014-05-15 13:03:25 +0400

    AlternativesCriterion in stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,15 +1,16 @@
 class StatsController < ApplicationController
   def index
     render content_type: 'text/plain', text: <<-"END"
-      Alternative:         #{Alternative.count}
-      Criterion:           #{Criterion.count}
-      Medium:              #{Medium.count}
-      Review:              #{Review.count}
-      ReviewSentence:      #{ReviewSentence.count}
-      Review::Tripadvisor: #{Review::Tripadvisor.count}
-      Property::Field:     #{Property::Field.count}
-      Property::Group:     #{Property::Group.count}
-      Property::Value:     #{Property::Value.count}
+      Alternative:              #{Alternative.count}
+      Criterion:                #{Criterion.count}
+      AlternativesCriterion:    #{AlternativesCriterion.count}
+      Medium:                   #{Medium.count}
+      Review:                   #{Review.count}
+      ReviewSentence:           #{ReviewSentence.count}
+      Review::Tripadvisor:      #{Review::Tripadvisor.count}
+      Property::Field:          #{Property::Field.count}
+      Property::Group:          #{Property::Group.count}
+      Property::Value:          #{Property::Value.count}
     END
   end
 end
