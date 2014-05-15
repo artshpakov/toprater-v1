5dd734399712e571dcaad49ba7280b15e30a9092
Sergey Besedin <kr3ssh@gmail.com>
2014-05-15 18:52:11 +0400

    hardcodepower, find presets by name or use random

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -8,8 +8,12 @@ class IndexController < ApplicationController
 
     @properties = Property::Field.all
     gon.rabl template: "app/views/properties/index.json.rabl", as: :properties
+ 
+    presets = Criterion.where(short_name: ['sea', 'boat trip', 'heathy food', 'chef']).pluck(:id)
+    presets = Criterion.order("RANDOM()").limit(4).pluck(:id) if presets.empty?
 
-    gon.preset = [[671, true], [467, false], [320, true], [209, true]]
+    gon.preset = presets.map{|pf| [pf, true]}
+
   end
 
 end
