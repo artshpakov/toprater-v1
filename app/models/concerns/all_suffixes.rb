1a6c282505db89d9347c02652631d8132728a376
codesnik <aronaxis@gmail.com>
2014-05-16 13:59:21 +0400

    Kids this days don't know no regexen

--- app/models/concerns/all_suffixes.rb
+++ app/models/concerns/all_suffixes.rb
@@ -4,6 +4,7 @@ private
 
   # "foo bar baz" => ["foo bar baz", "bar baz", "baz"]
   def all_suffixes(string)
-    string.scan(/(?:^|\s)(?=(\S.*))/).flatten
+    words = string.split
+    (0...words.size).map {|idx| words[idx..-1].join(" ") }
   end
 end
