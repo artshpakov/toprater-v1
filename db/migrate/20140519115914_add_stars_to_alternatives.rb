d6db09abfcf262b3457fc1f4730d67caa0a47605
pechorin <pechorin.andrey@gmail.com>
2014-05-19 17:27:28 +0400

    Revert "add #address and #stars to alternatives"
    This reverts commit 305c41fed0e5f81792664eb2758ef7292d1ba016.

--- db/migrate/20140519115914_add_stars_to_alternatives.rb
+++
@@ -1,5 +0,0 @@
-class AddStarsToAlternatives < ActiveRecord::Migration
-  def change
-    add_column :alternatives, :stars, :integer
-  end
-end
--- db/migrate/20140519130441_add_address_to_alternatives.rb
+++
@@ -1,5 +0,0 @@
-class AddAddressToAlternatives < ActiveRecord::Migration
-  def change
-    add_column :alternatives, :address, :text
-  end
-end
